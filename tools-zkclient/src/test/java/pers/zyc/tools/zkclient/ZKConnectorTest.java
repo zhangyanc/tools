7bee2d6ad2acdfa6e3cffd76ee1c19e7fb4c7e1c
zhangyancheng <zhangyancheng@jd.com>
2018-02-11 19:11:26 +0800

    connector main flow test

--- tools-zkclient/src/test/java/pers/zyc/tools/zkclient/ZKConnectorTest.java
+++ tools-zkclient/src/test/java/pers/zyc/tools/zkclient/ZKConnectorTest.java
@@ -1,5 +1,6 @@
 package pers.zyc.tools.zkclient;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,21 +8,23 @@ import pers.zyc.tools.zkclient.event.ConnectionEvent;
 import pers.zyc.tools.zkclient.listener.ConnectionListener;
 
 import java.util.LinkedList;
+import java.util.concurrent.Semaphore;
 
 /**
  * @author zhangyancheng
  */
 public class ZKConnectorTest {
-
-    private static String ZK_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 30000;
 
     private static class TestConnectorConnectionListener implements ConnectionListener {
 
+        Semaphore eventSemaphore = new Semaphore(0);
         LinkedList<ConnectionEvent> receivedEvents = new LinkedList<>();
 
         @Override
         public void onEvent(ConnectionEvent event) {
             receivedEvents.add(event);
+            eventSemaphore.release();
         }
 
         boolean unReceivedEvent() {
@@ -33,13 +36,22 @@ public class ZKConnectorTest {
         }
     }
 
+    private ZKSwitch zkSwitch;
     private ZKConnector connector;
     private TestConnectorConnectionListener testListener;
 
     @Before
     public void setUp() {
-        connector = new ZKConnector(ZK_ADDRESS, 30000);
+        connector = new ZKConnector("localhost:2181", SESSION_TIMEOUT);
+        zkSwitch = new ZKSwitch("E:/Tools/zookeeper-3.4.6");
         testListener = new TestConnectorConnectionListener();
+        zkSwitch.open();
+    }
+
+    @After
+    public void tearDown() {
+        connector.stop();
+        zkSwitch.close();
     }
 
     /**
@@ -47,15 +59,15 @@ public class ZKConnectorTest {
      */
     @Test
     public void case_unStarted() throws Exception {
-        Assert.assertFalse(connector.isConnected());
-        Assert.assertNull(connector.getZooKeeper());
+        Assert.assertNotNull(connector.getZooKeeper());
+        Assert.assertTrue(connector.isConnected());
     }
 
     /**
      * 测试事件发布主流程
      *
      * 测试步骤:
-     *        1. 运行测试前启动zk服务
+     *        1. 启动zk服务
      *        2. 添加监听器(不会收到事件)
      *        3. 启动connector(监听到CONNECTED事件)
      *        4. 关闭zk(监听到DISCONNECTED事件)
@@ -63,18 +75,45 @@ public class ZKConnectorTest {
      *        6. 关闭zk(监听到DISCONNECTED事件)
      *        7. 在sessionTimeout之前未重启zk(监听到SESSION_CLOSED事件)
      *        8. 重启zk(监听到CONNECTED事件)
+     *        9. 关闭connector(不会受到事件)
      */
     @Test
     public void case_mainFlow() throws Exception {
         connector.addListener(testListener);
-        Thread.sleep(200);
         Assert.assertTrue(testListener.unReceivedEvent());
 
         connector.start();
-        Thread.sleep(200);
+        testListener.eventSemaphore.acquire();
         Assert.assertEquals(ConnectionEvent.CONNECTED, testListener.lastEvent());
         Assert.assertTrue(connector.isConnected());
 
+        zkSwitch.close();
+        testListener.eventSemaphore.acquire();
+        Assert.assertEquals(ConnectionEvent.DISCONNECTED, testListener.lastEvent());
+        Assert.assertFalse(connector.isConnected());
 
+        Thread.sleep((long) (Math.random() * SESSION_TIMEOUT) - 1000);
+        zkSwitch.open();
+        testListener.eventSemaphore.acquire();
+        Assert.assertEquals(ConnectionEvent.RECONNECTED, testListener.lastEvent());
+        Assert.assertTrue(connector.isConnected());
+
+        zkSwitch.close();
+        testListener.eventSemaphore.acquire();
+        Assert.assertEquals(ConnectionEvent.DISCONNECTED, testListener.lastEvent());
+        Assert.assertFalse(connector.isConnected());
+
+
+        testListener.eventSemaphore.acquire();
+        Assert.assertEquals(ConnectionEvent.SESSION_CLOSED, testListener.lastEvent());
+        Assert.assertFalse(connector.isConnected());
+
+        zkSwitch.open();
+        testListener.eventSemaphore.acquire();
+        Assert.assertEquals(ConnectionEvent.CONNECTED, testListener.lastEvent());
+        Assert.assertTrue(connector.isConnected());
+
+        connector.stop();
+        Assert.assertFalse(connector.isConnected());
     }
 }
--- tools-zkclient/src/test/java/pers/zyc/tools/zkclient/ZKSwitch.java
+++ tools-zkclient/src/test/java/pers/zyc/tools/zkclient/ZKSwitch.java
@@ -1,8 +1,12 @@
 package pers.zyc.tools.zkclient;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 控制启动和关闭zk服务
@@ -10,57 +14,148 @@ import java.io.InputStreamReader;
  * @author zhangyancheng
  */
 class ZKSwitch {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ZKSwitch.class);
+
+    private interface CommandCallback<V> {
+        V execute(Process process);
+    }
+
+    private class WaitCallback implements CommandCallback<Void> {
+        private int sleep;
+
+        public WaitCallback(int sleep) {
+            this.sleep = sleep;
+        }
+
+        @Override
+        public Void execute(Process process) {
+            try {
+                TimeUnit.MILLISECONDS.sleep(sleep);
+            } catch (InterruptedException ignored) {
+            }
+            return null;
+        }
+    }
+
+    private class StartZooKeeperCallback implements CommandCallback<Integer> {
+
+        @Override
+        public Integer execute(Process process) {
+            try {
+                BufferedReader bufferedReader = new BufferedReader(
+                        new InputStreamReader(process.getInputStream(), "GBK"));
+                String line;
+                while ((line = bufferedReader.readLine()) != null){
+                    //System.out.println(line);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        }
+    }
+
     private String zkBaseDir;
-    private int zkPort;
     private int zkPid = -1;
 
-    ZKSwitch(String zkBaseDir, int zkPort) {
+    ZKSwitch(String zkBaseDir) {
         this.zkBaseDir = zkBaseDir;
-        this.zkPort = zkPort;
     }
 
-    private Process exec(String cmd) {
+    private static void exec(String cmd) {
+        exec(cmd, null);
+    }
+
+    private static <V> V exec(String cmd, CommandCallback<V> callback) {
+        Process process = null;
         try {
-            return Runtime.getRuntime().exec(cmd);
+            process = Runtime.getRuntime().exec(cmd);
+            return callback != null ? callback.execute(process) : null;
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            if (process != null) {
+                process.destroy();
+            }
         }
     }
 
-    private int getZooKeeperPid() {
-        Process process = exec("netstat -ano");
-        try {
-            BufferedReader bufferedReader = new BufferedReader(
-                    new InputStreamReader(process.getInputStream(), "GBK"));
+    private static Integer getPidByListenPort(final int listenPort) {
+        return exec("netstat -ano", new CommandCallback<Integer>() {
+            @Override
+            public Integer execute(Process process) {
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(
+                            new InputStreamReader(process.getInputStream(), "GBK"));
 
-            String line;
-            while ((line = bufferedReader.readLine()) != null){
-                if (line.contains(":" + zkPort)) {
-                    //[, TCP, 0.0.0.0:2181, 0.0.0.0:0, LISTENING, 409792]
-                    String[] array = line.split("\\s+");
-                    return Integer.parseInt(array[array.length - 1]);
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null){
+                        if (line.contains(":" + listenPort)) {
+                            //[, TCP, 0.0.0.0:2181, 0.0.0.0:0, LISTENING, 409792]
+                            String[] array = line.split("\\s+");
+                            return Integer.parseInt(array[array.length - 1]);
+                        }
+                    }
+                    throw new RuntimeException("Can not find zookeeper pid for listen port: " + listenPort);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
             }
-            throw new RuntimeException("Can not find zookeeper pid!");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        });
     }
 
-    private void closeZooKeeper(int zkWinPid) {
-        exec("taskkill /f /pid " + zkWinPid);
+    private static Integer getPidByProcessName(final String processName) {
+        return exec("jps -l", new CommandCallback<Integer>() {
+            @Override
+            public Integer execute(Process process) {
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(
+                            new InputStreamReader(process.getInputStream(), "GBK"));
+
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null){
+                        if (line.contains(processName)) {
+                            return Integer.parseInt(line.split(" ")[0]);
+                        }
+                    }
+                    throw new RuntimeException("Can not find zookeeper pid for process name: " + processName);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
     void open() {
-
-        zkPid = getZooKeeperPid();
+        Thread starter = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                exec(zkBaseDir + "/bin/zkServer.cmd", new StartZooKeeperCallback());
+            }
+        });
+        starter.start();
+        try {
+            starter.join(1000);
+        } catch (InterruptedException ignored) {
+        }
+        //获取zk server的进程pid
+        zkPid = getPidByProcessName("QuorumPeerMain");
+        LOGGER.info("ZooKeeper start success, pid: {}", zkPid);
     }
 
     void close() {
-        if (zkPid < 0) {
-            throw new RuntimeException("ZooKeeper not started!");
+        if (zkPid != -1) {
+            exec("taskkill /f /pid " + zkPid, new WaitCallback(1000));
+            LOGGER.info("ZooKeeper close success.");
+            zkPid = -1;
         }
-        exec("taskkill /f /pid " + zkPid);
-        zkPid = -1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        ZKSwitch zkSwitch = new ZKSwitch("E:/Tools/zookeeper-3.4.6");
+        zkSwitch.open();
+        System.out.println(zkSwitch.zkPid);
+        zkSwitch.close();
+        System.out.println(zkSwitch.zkPid);
     }
 }
