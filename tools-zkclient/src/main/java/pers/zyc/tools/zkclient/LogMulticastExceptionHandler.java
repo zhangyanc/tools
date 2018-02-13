73e60eedfc8d42ad7b0381738350ea43258a94e8
zhangyancheng <zhangyancheng@jd.com>
2018-02-13 17:47:41 +0800

    rename

--- tools-zkclient/src/main/java/pers/zyc/tools/zkclient/LogMulticastExceptionHandler.java
+++ tools-zkclient/src/main/java/pers/zyc/tools/zkclient/LogMulticastExceptionHandler.java
@@ -3,7 +3,7 @@ package pers.zyc.tools.zkclient;
 import org.slf4j.Logger;
 import pers.zyc.tools.event.Event;
 import pers.zyc.tools.event.EventListener;
-import pers.zyc.tools.event.DeliverExceptionHandler;
+import pers.zyc.tools.event.MulticastExceptionHandler;
 
 import java.util.Objects;
 
@@ -12,10 +12,10 @@ import java.util.Objects;
  *
  * @author zhangyancheng
  */
-public class LogDeliverExceptionHandler implements DeliverExceptionHandler {
+public class LogMulticastExceptionHandler implements MulticastExceptionHandler {
     private final Logger logger;
 
-    public LogDeliverExceptionHandler(Logger logger) {
+    public LogMulticastExceptionHandler(Logger logger) {
         this.logger = Objects.requireNonNull(logger);
     }
 
