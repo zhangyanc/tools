41acd77f5b3bcb7c8268341f96a51f4389b7b020
zhangyancheng <zhangyancheng@jd.com>
2018-03-09 22:30:51 +0800

    事件派发重构

--- tools-event/src/main/java/pers/zyc/tools/event/EventBus.java
+++ tools-event/src/main/java/pers/zyc/tools/event/EventBus.java
@@ -4,180 +4,310 @@ import pers.zyc.tools.lifecycle.PeriodicService;
 import pers.zyc.tools.utils.Pair;
 import pers.zyc.tools.utils.TimeMillis;
 
-import java.util.Objects;
-import java.util.Set;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CopyOnWriteArraySet;
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 事件管理器
+ * 事件异步派发
+ *
+ * @param <E> 事件泛型
  *
  * @author zhangyancheng
  */
-public final class EventBus<E> extends PeriodicService implements Listenable<EventHandler<E>> {
-    private String name;
-    private long idleTime;
-    private long mergeInterval;
-    private long lastEventTime;
-    private IdleCallback idleCallback;
-    private Set<EventHandler<E>> eventHandlers = new CopyOnWriteArraySet<>();
-    private BlockingQueue<Ownership> eventQueue = new LinkedBlockingDeque<>();
+public class EventBus<E> extends PeriodicService implements Listenable<EventListener<E>> {
+	/**
+	 * 派发线程名
+	 */
+	private String name;
+	/**
+	 * 空闲时间(ms), 超过空闲时间没有事件触发onIdle
+	 */
+	private long idleTime;
+	/**
+	 * 事件合并周期(ms), 周期内如果有多个事件, 默认只发布最后一个
+	 */
+	private long mergeInterval;
+	/**
+	 * 派发线程单次从阻塞队列中获取事件的超时时间(ms)
+	 */
+	private long internalPollTimeout = 1000;
 
-    private EventBus(String name,
-                     long idleTime,
-                     long mergeInterval,
-                     IdleCallback idleCallback) {
+	/**
+	 * 事件队列长度
+	 */
+	private int eventQueueCapacity = Integer.MAX_VALUE;
 
-        this.name = name;
-        this.idleTime = idleTime;
-        this.mergeInterval = mergeInterval;
-        this.idleCallback = idleCallback;
+	/**
+	 * 上次触发事件
+	 */
+	private long lastEventTime;
+	/**
+	 * 事件队列, 如果队列满则阻塞add线程
+	 */
+	private BlockingQueue<Ownership> eventQueue;
+	
+	/**
+	 * 保存onEvent的方法引用, 用于出错处理
+	 */
+	private static final Method ON_EVENT_METHOD = EventListener.class.getMethods()[0];
+	
+	/**
+	 * 事件广播器
+	 */
+	private final Multicaster<EventListener> multicaster = new Multicaster<EventListener>(){};
+
+	@Override
+	public String getName() {
+		return name != null ? name : super.getName();
+	}
+
+	@Override
+	public void addListener(EventListener<E> listener) {
+		multicaster.addListener(listener);
+	}
+
+	@Override
+	public void removeListener(EventListener<E> listener) {
+		multicaster.removeListener(listener);
+	}
+
+	@Override
+	protected void doStart() {
+		eventQueue = new ArrayBlockingQueue<>(eventQueueCapacity);
+		lastEventTime = TimeMillis.get();
+		super.doStart();
+	}
+
+	@Override
+	protected void doStop() throws Exception {
+		eventQueue.clear();
+		super.doStop();
+	}
+
+	@Override
+	protected long period() {
+		return mergeInterval;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	protected void execute() throws InterruptedException {
+		//此轮处理的event, 还用于判断是否空闲
+		Ownership event = null;
+		
+		if (mergeInterval > 0) {
+            MergedEvents mergedEvents = new MergedEvents();
+            eventQueue.drainTo(mergedEvents);
+            
+            if (eventQueue.drainTo(mergedEvents) > 0) {
+                List<Ownership> events = mergedEvents.events;
+                for (int i = 0; i < events.size(); i++) {
+                    event = events.get(i);
+                    //当前是专有事件则直接发布, 否则比较是否最后一个非专有事件, 如果是则发布
+                    if (event.value() != null) {
+                        inform(event.key(), event.value());
+                    } else if (mergedEvents.mergedEventIndex == i) {
+                        multicaster.listeners.onEvent(event.key());
+                    }
+                }
+            }
+		} else {
+			event = eventQueue.poll(internalPollTimeout, TimeUnit.MILLISECONDS);
+			
+			if (event != null) {
+                if (event.value() != null) {
+                    inform(event.key(), event.value());
+                } else {
+                    multicaster.listeners.onEvent(event.key());
+                }
+            }
+		}
+		
+		if (event == null && idleTime > 0 && (TimeMillis.get() - lastEventTime) >= idleTime) {
+            lastEventTime = TimeMillis.get();
+            onIdle();
+        }
+	}
+	
+	/**
+	 * 专有事件通知
+	 * @param event 事件
+	 * @param eventOwner 事件专有监听器
+	 */
+	protected void inform(E event, EventListener<E> eventOwner) {
+        try {
+            eventOwner.onEvent(event);
+        } catch (Throwable throwable) {
+        	//异常后加入出错处理
+            if (multicaster.getExceptionHandler() != null) {
+	            try {
+		            multicaster.getExceptionHandler().handleException(throwable,
+				            eventOwner, ON_EVENT_METHOD, new Object[]{ event });
+	            } catch (Throwable ignored) {
+	            }
+            }
+        }
     }
+	
+	/**
+	 * 空闲, 由子类制定逻辑
+	 */
+	protected void onIdle() {
+	}
 
-    @Override
-    public String getName() {
-        return name != null ? name : super.getName();
-    }
+	/**
+	 * 添加事件, 如果队列满则阻塞至队列空闲或者被中断
+	 *
+	 * @param event 待添加事件
+	 * @throws InterruptedException 等待过程中被中断
+     * @throws NullPointerException event为null时抛出
+	 */
+	public void add(E event) throws InterruptedException {
+		add(event, null);
+	}
 
-    @Override
-    protected void afterStart() {
-        super.afterStart();
-        lastEventTime = TimeMillis.get();
-    }
-
-    @Override
-    protected void doStop() throws Exception {
-        super.doStop();
+	/**
+	 * 添加事件, 如果队列满则阻塞至队列空闲或者被中断
+	 *
+	 * @param event 待添加事件
+	 * @param owner 事件所属监听器, 如果为null表示事件通知给所有监听器
+	 * @throws InterruptedException 等待过程中被中断
+     * @throws NullPointerException event为null时抛出
+	 */
+	public void add(E event, EventListener<E> owner) throws InterruptedException {
+		eventQueue.put(new Ownership(Objects.requireNonNull(event), owner));
+	}
+    
+    /**
+     * 添加事件, 如果当前队列满则返回添加失败
+     *
+     * @param event 待添加事件
+     * @return 是否添加成功
+     * @throws NullPointerException event为null时抛出
+     */
+    public boolean offer(E event) {
+		return offer(event, null);
+	}
+    
+    /**
+     * 添加事件, 如果当前队列满则返回添加失败
+     *
+     * @param event 待添加事件
+     * @param owner 事件所属监听器, 如果为null表示事件通知给所有监听器
+     * @return 是否添加成功
+     * @throws NullPointerException event为null时抛出
+     */
+    public boolean offer(E event, EventListener<E> owner) {
+		return eventQueue.offer(new Ownership(Objects.requireNonNull(event), owner));
+	}
+    
+    /**
+     * 清空事件队列
+     */
+    public void clearEvents() {
         eventQueue.clear();
     }
 
-    @Override
-    protected long period() {
-        return mergeInterval;
-    }
+	/**
+	 * 设置EventBus名称, 同时也是派发线程名
+	 */
+	public EventBus<E> name(String name) {
+		this.name = name;
+		return this;
+	}
 
-    @Override
-    protected void execute() throws InterruptedException {
-        Ownership event = null;
+	/**
+	 * 设置事件队列容量
+	 * @param eventQueueCapacity 事件队列容量, 默认为无界队列
+	 */
+	public EventBus<E> eventQueueCapacity(int eventQueueCapacity) {
+		this.eventQueueCapacity = eventQueueCapacity;
+		return this;
+	}
 
-        if (mergeInterval > 0) {
-            int size = eventQueue.size();
-            while (size-- > 0) {
-                event = eventQueue.poll();
+	/**
+	 * 设置空闲时间, 空闲时间段内无事件触发空闲
+	 * @param idleTime 空闲时间(ms), 默认不触发空闲
+	 */
+	public EventBus<E> idleTime(long idleTime) {
+		this.idleTime = idleTime;
+		return this;
+	}
+
+	/**
+	 * 设置事件合并间隔(ms), 间隔段内如果有多个事件默认只发布最后一个
+	 * @param mergeInterval 事件合并间隔(ms), 默认不进行事件合并
+	 */
+	public EventBus<E> mergeInterval(long mergeInterval) {
+		this.mergeInterval = mergeInterval;
+		return this;
+	}
+
+	/**
+	 * 设置派发线程单次从阻塞队列中获取事件的超时时间
+	 * @param internalPollTimeout 派发线程单次从阻塞队列中获取事件的超时时间(ms), 默认为1000ms
+	 */
+	public EventBus<E> internalPollTimeout(long internalPollTimeout) {
+		this.internalPollTimeout = internalPollTimeout;
+		return this;
+	}
+
+	/**
+	 * 设置事件广播执行器
+	 * @param multicastExecutor 广播执行器, 默认为同步执行器
+	 * @see Multicaster
+	 */
+	public EventBus<E> multicastExecutor(Executor multicastExecutor) {
+		this.multicaster.setMulticastExecutor(multicastExecutor);
+		return this;
+	}
+
+	/**
+	 * 设置事件广播异常处理器
+	 * @param multicastExceptionHandler 事件广播异常处理器, 默认不处理异常
+	 */
+	public EventBus<E> multicastExceptionHandler(MulticastExceptionHandler multicastExceptionHandler) {
+		this.multicaster.setExceptionHandler(multicastExceptionHandler);
+		return this;
+	}
+
+	/**
+	 * 事件-监听器元组
+	 */
+	private class Ownership extends Pair<E, EventListener<E>> {
+		Ownership(E event, EventListener<E> listener) {
+			key(event);
+			value(listener);
+		}
+	}
+	
+	private class MergedEvents extends AbstractCollection<Ownership> {
+		//最后一个非专有事件下标
+        int mergedEventIndex;
+        List<Ownership> events = new ArrayList<>();
+        
+        @Override
+        public boolean add(Ownership ownership) {
+            //合并不针对专属事件
+            if (ownership.value() == null) {
+                mergedEventIndex = events.size();
             }
-        } else {
-            event = eventQueue.poll(200, TimeUnit.MILLISECONDS);
+            events.add(ownership);
+            return true;
         }
-
-        if (event != null) {
-            lastEventTime = TimeMillis.get();
-            EventHandler<E> eventOwner = event.value();
-            E realEvent = event.key();
-            if (eventOwner != null) {
-                inform(eventOwner, realEvent);
-            } else {
-                for (EventHandler<E> eventHandler : eventHandlers) {
-                    inform(eventHandler, realEvent);
-                }
-            }
-        } else if (idleTime > 0 && idleCallback != null &&
-                (TimeMillis.get() - lastEventTime) >= idleTime) {
-
-            lastEventTime = TimeMillis.get();
-            idleCallback.onIdle();
+        
+        @Override
+        public Iterator<Ownership> iterator() {
+            throw new UnsupportedOperationException();
         }
-    }
-
-    private void inform(EventHandler<E> eventHandler, E event) {
-        try {
-            eventHandler.handleEvent(event);
-        } catch (Exception ignored) {
-        }
-    }
-
-    @Override
-    public void addListener(EventHandler<E> listener) {
-        eventHandlers.add(Objects.requireNonNull(listener));
-    }
-
-    @Override
-    public void removeListener(EventHandler<E> listener) {
-        eventHandlers.remove(listener);
-    }
-
-    /**
-     * 添加事件
-     *
-     * @param event 事件
-     */
-    public void add(E event) {
-        add(event, null);
-    }
-
-    /**
-     * 添加事件
-     *
-     * @param event 事件
-     * @param owner 事件所属监听器, 如果为null表示事件通知给所有监听器
-     */
-    public void add(E event, EventHandler<E> owner) {
-        Objects.requireNonNull(event);
-
-        Ownership ownership = new Ownership();
-        ownership.key(event);
-        ownership.value(owner);
-
-        eventQueue.offer(ownership);
-    }
-
-    private class Ownership extends Pair<E, EventHandler<E>> {
-    }
-
-    public interface IdleCallback {
-        void onIdle();
-    }
-
-    public static class Builder {
-        private String name;//事件管理器名
-        private long idleTime;
-        private long mergeInterval;
-        private IdleCallback idleCallback;
-
-        /**
-         * 设置事件管理器名称
-         */
-        public Builder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        /**
-         * 连续无事件表示空闲, 空闲事件超时触发空闲回调
-         *
-         * @param idleTime 空闲时间
-         * @param idleCallback 空闲回调
-         */
-        public Builder idle(long idleTime, IdleCallback idleCallback) {
-            this.idleTime = idleTime;
-            this.idleCallback = idleCallback;
-            return this;
-        }
-
-        /**
-         * 设置事件合并间隔, 间隔段内的事件将只发布最后一个
-         */
-        public Builder mergeInterval(long mergeInterval) {
-            this.mergeInterval = mergeInterval;
-            return this;
-        }
-
-        /**
-         * 创建事件管理器
-         */
-        public <E> EventBus<E> build() {
-            return new EventBus<>(name, idleTime, mergeInterval, idleCallback);
+        
+        @Override
+        public int size() {
+            throw new UnsupportedOperationException();
         }
     }
 }
