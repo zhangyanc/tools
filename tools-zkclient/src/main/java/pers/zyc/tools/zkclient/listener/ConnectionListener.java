d7a613303ca64b21bbb110ea127c2782f069defd
zhangyancheng <zhangyancheng@jd.com>
2018-03-09 21:26:34 +0800

    修改方法

--- tools-zkclient/src/main/java/pers/zyc/tools/zkclient/listener/ConnectionListener.java
+++ tools-zkclient/src/main/java/pers/zyc/tools/zkclient/listener/ConnectionListener.java
@@ -1,10 +1,28 @@
 package pers.zyc.tools.zkclient.listener;
 
-import pers.zyc.tools.event.EventListener;
-import pers.zyc.tools.zkclient.event.ConnectionEvent;
+import pers.zyc.tools.event.Listener;
 
 /**
  * @author zhangyancheng
  */
-public interface ConnectionListener extends EventListener<ConnectionEvent> {
+public interface ConnectionListener extends Listener {
+    /**
+     * 连接成功, 新的ZooKeeper实例连接成功
+     */
+    void onConnected();
+
+    /**
+     * 重连成功(由suspend状态恢复)
+     */
+    void onReconnected();
+
+    /**
+     * 断线, 相当于Disconnected
+     */
+    void onSuspend();
+
+    /**
+     * 会话超时被关闭
+     */
+    void onSessionClosed();
 }
