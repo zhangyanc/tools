e582ecc2d46ff88f4b9591c0d595aad227dc91b1
zhangyancheng <zhangyancheng@jd.com>
2018-06-25 14:21:09 +0800

    添加自定义中断方法(对于某些不响应中断的操作可及时响应)

--- tools-lifecycle/src/main/java/pers/zyc/tools/lifecycle/PeriodicService.java
+++ tools-lifecycle/src/main/java/pers/zyc/tools/lifecycle/PeriodicService.java
@@ -8,68 +8,73 @@ import java.util.concurrent.TimeUnit;
  * @author zhangyancheng
  */
 public abstract class PeriodicService extends Service implements Thread.UncaughtExceptionHandler {
-    private Thread periodicThread;
-    private final PeriodicRunnable periodicRunnable = new PeriodicRunnable();
+	private Thread periodicThread;
 
-    @Override
-    protected void doStart() {
-    }
+	@Override
+	protected void doStart() {
+		periodicThread = new PeriodicExecuteThread();
+	}
 
-    @Override
-    protected void afterStart() {
-        periodicThread = new Thread(periodicRunnable, getName());
-        periodicThread.setUncaughtExceptionHandler(this);
-        periodicThread.setDaemon(true);
-        periodicThread.start();
-    }
+	@Override
+	protected void afterStart() {
+		periodicThread.start();
+	}
 
-    @Override
-    protected void doStop() throws Exception {
-        periodicThread.interrupt();
-    }
+	@Override
+	protected void doStop() throws Exception {
+		periodicThread.interrupt();
+	}
 
-    /**
-     * 守护线程结束时可在这里记录异常
-     *
-     * @param t 线程
-     * @param e 异常
-     */
-    @Override
-    public void uncaughtException(Thread t, Throwable e) {
-        stop();
-    }
+	/**
+	 * 守护线程异常结束时可在这里记录或关闭服务
+	 *
+	 * @param t 线程
+	 * @param e 异常
+	 */
+	@Override
+	public void uncaughtException(Thread t, Throwable e) {
+		stop();
+	}
 
-    /**
-     * @return 休眠周期
-     */
-    protected abstract long period();
+	/**
+	 * @return 休眠周期
+	 */
+	protected abstract long period();
 
-    /**
-     * @return 检查存活
-     */
-    protected boolean isAlive() {
-        return isRunning() && !periodicThread.isInterrupted();
-    }
+	/**
+	 * 周期执行逻辑
+	 *
+	 * @throws InterruptedException 线程被打断
+	 */
+	protected abstract void execute() throws InterruptedException;
 
-    /**
-     * 周期执行逻辑
-     *
-     * @throws InterruptedException 线程被打断
-     */
-    protected abstract void execute() throws InterruptedException;
+	protected void onInterrupt() {
+	}
 
-    private class PeriodicRunnable implements Runnable {
+	private class PeriodicExecuteThread extends Thread {
 
-        @Override
-        public void run() {
-            while (isAlive()) {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(period());
-                    execute();
-                } catch (InterruptedException e) {
-                    break;
-                }
-            }
-        }
-    }
+		{
+			setName(getName());
+			setDaemon(true);
+			setUncaughtExceptionHandler(PeriodicService.this);
+		}
+
+		@Override
+		public void run() {
+			while (isRunning() && !isInterrupted()) {
+				try {
+					TimeUnit.MILLISECONDS.sleep(period());
+					execute();
+				} catch (InterruptedException e) {
+					break;
+				}
+			}
+		}
+
+		@Override
+		public void interrupt() {
+			onInterrupt();
+			super.interrupt();
+		}
+	}
 }
