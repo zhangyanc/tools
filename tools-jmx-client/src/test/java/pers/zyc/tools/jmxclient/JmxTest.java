40b1cf07527b4f77e3866b7d91a473f62c5ff58c
zhangyancheng <zhangyancheng@jd.com>
2019-03-20 19:58:52 +0800

    test

--- tools-jmx-client/src/test/java/pers/zyc/tools/jmxclient/JmxTest.java
+++ tools-jmx-client/src/test/java/pers/zyc/tools/jmxclient/JmxTest.java
@@ -4,8 +4,10 @@ import com.sun.management.GarbageCollectorMXBean;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pers.zyc.tools.utils.event.EventListener;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @author zhangyancheng
@@ -14,17 +16,26 @@ public class JmxTest {
 	private static final Logger LOGGER = LoggerFactory.getLogger(JmxTest.class);
 
 	@Test
-	public void case_0() {
+	public void case_0() throws InterruptedException {
 		JmxHost host = new JmxHost("172.25.46.10", 10088);
 		JmxUser user = new JmxUser("admin", "admin");
 
+		final CountDownLatch cdl = new CountDownLatch(2);
 		JmxClient jmxClient = new JmxClient(host, user);
-		LOGGER.info("{}", jmxClient.getConnection());
+		jmxClient.addListener(new EventListener<ConnectionEvent>() {
+			@Override
+			public void onEvent(ConnectionEvent event) {
+				cdl.countDown();
+				LOGGER.info(event.toString());
+			}
+		});
+		jmxClient.connect();
 
 		List<GarbageCollectorMXBean> gcMXBeans = JvmStandardMXBeans.getGarbageCollectorMXBean(jmxClient);
 		for (GarbageCollectorMXBean gcMXBean : gcMXBeans) {
 			LOGGER.info("{}, {} {}", gcMXBean.getName(), gcMXBean.getCollectionCount(), gcMXBean.getCollectionTime());
 		}
-		jmxClient.close();
+		jmxClient.disconnect();
+		cdl.await();
 	}
 }
