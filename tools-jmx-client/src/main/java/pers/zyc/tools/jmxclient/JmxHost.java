30232bc2ca151e6286cb2366cdf50087d6fe827b
zhangyancheng <zhangyancheng@jd.com>
2019-03-21 14:06:46 +0800

    通过ip:port串创建JmxHost

--- tools-jmx-client/src/main/java/pers/zyc/tools/jmxclient/JmxHost.java
+++ tools-jmx-client/src/main/java/pers/zyc/tools/jmxclient/JmxHost.java
@@ -1,6 +1,7 @@
 package pers.zyc.tools.jmxclient;
 
 import pers.zyc.tools.utils.Pair;
+import pers.zyc.tools.utils.Regex;
 
 /**
  * @author zhangyancheng
@@ -8,6 +9,9 @@ import pers.zyc.tools.utils.Pair;
 public class JmxHost extends Pair<String, Integer> {
 
 	public JmxHost(String ip, int port) {
+		if (!Regex.IPV4.matches(ip)) {
+			throw new IllegalArgumentException("Invalid ip: " + ip);
+		}
 		key(ip);
 		value(port);
 	}
@@ -22,5 +26,15 @@ public class JmxHost extends Pair<String, Integer> {
 
 	public String getHost() {
 		return getIp() + ":" + getPort();
+	}
+
+	/**
+	 * 通过ip:port串创建JmxHost
+	 *
+	 * @param host ip:port字符串
+	 */
+	public static JmxHost parse(String host) {
+		String[] part = host.split(":");
+		return new JmxHost(part[0], Integer.parseInt(part[1]));
 	}
 }
